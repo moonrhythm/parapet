4ed8cb56a8201018a9fdd2b5a8ea444c2aa7dbe8
acoshift <acoshift@gmail.com>
2018-12-02 01:18:13 +0700

    add hsts

---
+++ pkg/hsts/hsts.go
@@ -0,0 +1,48 @@
+package hsts
+
+import (
+	"net/http"
+	"strconv"
+	"time"
+)
+
+// HSTS middleware
+type HSTS struct {
+	MaxAge            time.Duration
+	IncludeSubDomains bool
+	Preload           bool
+}
+
+// Default returns default hsts
+func Default() *HSTS {
+	return &HSTS{
+		MaxAge:            31536000 * time.Second,
+		IncludeSubDomains: false,
+		Preload:           false,
+	}
+}
+
+// Preload returns hsts preload
+func Preload() *HSTS {
+	return &HSTS{
+		MaxAge:            63072000 * time.Second,
+		IncludeSubDomains: true,
+		Preload:           true,
+	}
+}
+
+// ServeHandler implements middleware interface
+func (m *HSTS) ServeHandler(h http.Handler) http.Handler {
+	hs := "max-age=" + strconv.FormatInt(int64(m.MaxAge/time.Second), 10)
+	if m.IncludeSubDomains {
+		hs += "; includeSubDomains"
+	}
+	if m.Preload {
+		hs += "; preload"
+	}
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Strict-Transport-Security", hs)
+		h.ServeHTTP(w, r)
+	})
+}
