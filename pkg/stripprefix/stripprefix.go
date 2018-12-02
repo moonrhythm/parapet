1c69daefb6ea9af91bbd03ac1289b468861e1026
acoshift <acoshift@gmail.com>
2018-12-02 19:36:05 +0700

    stripprefix: add new

--- pkg/stripprefix/stripprefix.go
+++ pkg/stripprefix/stripprefix.go
@@ -7,6 +7,11 @@ type StripPrefix struct {
 	Prefix string
 }
 
+// New creates new strip prefix middleware
+func New(prefix string) *StripPrefix {
+	return &StripPrefix{Prefix: prefix}
+}
+
 // ServeHandler implements middleware interface
 func (m *StripPrefix) ServeHandler(h http.Handler) http.Handler {
 	return http.StripPrefix(m.Prefix, h)
