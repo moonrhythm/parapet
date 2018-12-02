b59879a4ef20baa01b1c08c1f95cff3077feb34c
acoshift <acoshift@gmail.com>
2018-12-02 21:07:54 +0700

    add healthz

---
+++ pkg/healthz/healthz.go
@@ -0,0 +1,19 @@
+package healthz
+
+import "net/http"
+
+// Healthz middleware
+type Healthz struct{}
+
+// New creates new healthz
+func New() *Healthz {
+	return new(Healthz)
+}
+
+// ServeHandler implements middleware interface
+func (m *Healthz) ServeHandler(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+}
