11d05f083106c9a8a031b8e1b1f378eb4037f737
Thanatat Tamtan <acoshift@gmail.com>
2018-12-05 20:59:35 +0700

    headers: add map response (#8)
    * headers: add map response
    
    * headers: always run mapping

--- pkg/headers/map.go
+++ pkg/headers/map.go
@@ -50,3 +50,83 @@ func (m *RequestMapper) ServeHandler(h http.Handler) http.Handler {
 		h.ServeHTTP(w, r)
 	})
 }
+
+// MapResponse creates new response mapper
+func MapResponse(header string, mapper func(string) string) *ResponseMapper {
+	return &ResponseMapper{
+		Header: header,
+		Mapper: mapper,
+	}
+}
+
+// ResponseMapper maps a response header
+type ResponseMapper struct {
+	Header string
+	Mapper func(string) string
+}
+
+// ServeHandler implements middleware interface
+func (m *ResponseMapper) ServeHandler(h http.Handler) http.Handler {
+	if m.Header == "" || m.Mapper == nil {
+		return h
+	}
+
+	key := textproto.CanonicalMIMEHeaderKey(m.Header)
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		nw := responseMapperRW{
+			ResponseWriter: w,
+			mapHeader:      key,
+			mapper:         m.Mapper,
+		}
+		defer nw.mapping()
+
+		h.ServeHTTP(&nw, r)
+	})
+}
+
+type responseMapperRW struct {
+	http.ResponseWriter
+	wroteHeader bool
+	mapHeader   string
+	mapper      func(string) string
+}
+
+func (w *responseMapperRW) mapping() {
+	if w.wroteHeader {
+		return
+	}
+
+	h := w.Header()
+	hh := h[w.mapHeader]
+	if len(hh) == 0 {
+		return
+	}
+
+	delete(h, w.mapHeader)
+	for _, v := range hh {
+		x := w.mapper(v)
+		if x != "" {
+			h[w.mapHeader] = append(h[w.mapHeader], x)
+		}
+	}
+}
+
+func (w *responseMapperRW) WriteHeader(statusCode int) {
+	if w.wroteHeader {
+		return
+	}
+
+	w.mapping()
+
+	w.wroteHeader = true
+	w.ResponseWriter.WriteHeader(statusCode)
+}
+
+func (w *responseMapperRW) Write(p []byte) (int, error) {
+	if !w.wroteHeader {
+		w.WriteHeader(http.StatusOK)
+	}
+
+	return w.ResponseWriter.Write(p)
+}
