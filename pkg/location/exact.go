34412879ee95562158e51d357892898f7cf80a55
acoshift <acoshift@gmail.com>
2018-12-10 22:27:34 +0700

    rename

--- pkg/location/exact.go
+++ pkg/location/exact.go
@@ -6,24 +6,24 @@ import (
 	"github.com/moonrhythm/parapet"
 )
 
-// Match creates new matcher
-func Match(pattern string) *Matcher {
-	return &Matcher{Pattern: pattern}
+// Exact creates new exact matcher
+func Exact(pattern string) *ExactMatcher {
+	return &ExactMatcher{Pattern: pattern}
 }
 
-// Matcher matchs exact location
-type Matcher struct {
+// ExactMatcher matchs exact location
+type ExactMatcher struct {
 	Pattern string
 	ms      parapet.Middlewares
 }
 
 // Use uses middleware
-func (l *Matcher) Use(m parapet.Middleware) {
+func (l *ExactMatcher) Use(m parapet.Middleware) {
 	l.ms.Use(m)
 }
 
 // ServeHandler implements middleware interface
-func (l *Matcher) ServeHandler(h http.Handler) http.Handler {
+func (l *ExactMatcher) ServeHandler(h http.Handler) http.Handler {
 	next := l.ms.ServeHandler(http.NotFoundHandler())
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
