8c693d2af9ebc1e0b1199a6e55f1636ced55b059
Thanatat Tamtan <acoshift@gmail.com>
2018-12-10 16:51:00 +0700

    stackdriver/trace: add more config (#27)

--- pkg/stackdriver/trace/trace.go
+++ pkg/stackdriver/trace/trace.go
@@ -3,10 +3,13 @@ package trace
 import (
 	"log"
 	"net/http"
+	"time"
 
 	"contrib.go.opencensus.io/exporter/stackdriver"
+	sdpropagation "go.opencensus.io/exporter/stackdriver/propagation"
 	"go.opencensus.io/plugin/ochttp"
 	"go.opencensus.io/trace"
+	"go.opencensus.io/trace/propagation"
 )
 
 // New creates new trace middleware
@@ -16,14 +19,37 @@ func New() *Trace {
 
 // Trace middleware
 type Trace struct {
-	ProjectID        string
-	IsPublicEndpoint bool
+	ProjectID            string
+	Propagation          propagation.HTTPFormat
+	BundleCountThreshold int
+	BundleDelayThreshold time.Duration
+	IsPublicEndpoint     bool
+	FormatSpanName       func(r *http.Request) string
+	StartOptions         trace.StartOptions
 }
 
 // ServeHandler implements middleware interface
 func (m *Trace) ServeHandler(h http.Handler) http.Handler {
+	if m.Propagation == nil {
+		m.Propagation = &sdpropagation.HTTPFormat{}
+	}
+	if m.FormatSpanName == nil {
+		m.FormatSpanName = func(r *http.Request) string {
+			proto := r.Header.Get("X-Forwarded-Proto")
+			return proto + "://" + r.Host + r.RequestURI
+		}
+	}
+	if m.StartOptions.Sampler == nil {
+		m.StartOptions.Sampler = trace.AlwaysSample()
+	}
+	if m.StartOptions.SpanKind == trace.SpanKindUnspecified {
+		m.StartOptions.SpanKind = trace.SpanKindServer
+	}
+
 	exporter, err := stackdriver.NewExporter(stackdriver.Options{
-		ProjectID: m.ProjectID,
+		ProjectID:            m.ProjectID,
+		BundleCountThreshold: m.BundleCountThreshold,
+		BundleDelayThreshold: m.BundleDelayThreshold,
 	})
 	if err != nil {
 		log.Println("stackdriver/trace:", err)
@@ -33,15 +59,10 @@ func (m *Trace) ServeHandler(h http.Handler) http.Handler {
 	trace.RegisterExporter(exporter)
 
 	return &ochttp.Handler{
-		Handler: h,
-		FormatSpanName: func(r *http.Request) string {
-			proto := r.Header.Get("X-Forwarded-Proto")
-			return proto + "://" + r.Host + r.RequestURI
-		},
-		StartOptions: trace.StartOptions{
-			Sampler:  trace.AlwaysSample(),
-			SpanKind: trace.SpanKindServer,
-		},
+		Handler:          h,
+		Propagation:      m.Propagation,
+		FormatSpanName:   m.FormatSpanName,
+		StartOptions:     m.StartOptions,
 		IsPublicEndpoint: m.IsPublicEndpoint,
 	}
 }
