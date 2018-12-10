8ebfbf8e8efa8a830bd88159fa75e07f17efd384
Thanatat Tamtan <acoshift@gmail.com>
2018-12-10 20:29:00 +0700

    add authn, basic authn (#29)
    * add authn, basic authn
    
    * authn: rename

---
+++ pkg/authn/authn.go
@@ -0,0 +1,30 @@
+package authn
+
+import (
+	"net/http"
+)
+
+// Authenticator middleware
+type Authenticator struct {
+	Type          string
+	Authenticator func(*http.Request) bool
+}
+
+// ServeHandler implements middleware interface
+func (m *Authenticator) ServeHandler(h http.Handler) http.Handler {
+	if m.Authenticator == nil {
+		return h
+	}
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !m.Authenticator(r) {
+			if m.Type != "" {
+				w.Header().Set("WWW-Authenticate", m.Type)
+			}
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		h.ServeHTTP(w, r)
+	})
+}
