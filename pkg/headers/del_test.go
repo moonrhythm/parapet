45561466e81637e410bcbf6fb30ce9f0f809d89a
acoshift <acoshift@gmail.com>
2019-01-26 22:07:16 +0700

    headers: add delete tests

--- pkg/headers/del_test.go
+++ pkg/headers/del_test.go
@@ -10,76 +10,84 @@ import (
 	. "github.com/moonrhythm/parapet/pkg/headers"
 )
 
-func TestRequestAdder(t *testing.T) {
+func TestRequestDeleter(t *testing.T) {
 	t.Parallel()
 
 	t.Run("Empty", func(t *testing.T) {
 		r := httptest.NewRequest("GET", "/", nil)
 		w := httptest.NewRecorder()
 		called := false
-		AddRequest().ServeHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		DeleteRequest().ServeHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			called = true
 		})).ServeHTTP(w, r)
 		assert.True(t, called)
 	})
 
-	t.Run("Add", func(t *testing.T) {
+	t.Run("Del", func(t *testing.T) {
 		r := httptest.NewRequest("GET", "/", nil)
+		r.Header.Set("X-Header", "1")
 		w := httptest.NewRecorder()
 		called := false
-		AddRequest("X-Header", "1", "X-Header", "2").ServeHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		DeleteRequest("X-Header").ServeHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			called = true
-			if assert.Len(t, r.Header["X-Header"], 2) {
-				assert.Equal(t, "1", r.Header["X-Header"][0])
-				assert.Equal(t, "2", r.Header["X-Header"][1])
-			}
+			assert.Empty(t, r.Header["X-Header"])
 		})).ServeHTTP(w, r)
 		assert.True(t, called)
 	})
 }
 
-func TestResponseAdder(t *testing.T) {
+func TestResponseDeleter(t *testing.T) {
 	t.Parallel()
 
 	t.Run("Empty", func(t *testing.T) {
 		r := httptest.NewRequest("GET", "/", nil)
 		w := httptest.NewRecorder()
 		called := false
-		AddResponse().ServeHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		DeleteResponse().ServeHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			called = true
 			w.WriteHeader(http.StatusOK)
 		})).ServeHTTP(w, r)
 		assert.True(t, called)
 	})
 
-	t.Run("Add", func(t *testing.T) {
+	t.Run("Write After Delete", func(t *testing.T) {
 		r := httptest.NewRequest("GET", "/", nil)
 		w := httptest.NewRecorder()
 		called := false
-		AddResponse("X-Header", "1", "X-Header", "2").ServeHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		DeleteResponse("X-Header").ServeHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			called = true
-			w.Header().Set("X-Header", "0")
+			w.Header().Set("X-Header", "1")
 			w.WriteHeader(http.StatusOK)
 		})).ServeHTTP(w, r)
 		assert.True(t, called)
-		if assert.Len(t, w.Header()["X-Header"], 3) {
-			assert.Equal(t, "0", w.Header()["X-Header"][0])
-			assert.Equal(t, "1", w.Header()["X-Header"][1])
-			assert.Equal(t, "2", w.Header()["X-Header"][2])
-		}
+		assert.Empty(t, w.Header()["X-Header"])
+	})
+
+	t.Run("Write Before Delete", func(t *testing.T) {
+		r := httptest.NewRequest("GET", "/", nil)
+		w := httptest.NewRecorder()
+		w.Header().Set("X-Header", "1")
+		called := false
+		DeleteResponse("X-Header").ServeHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			called = true
+			w.WriteHeader(http.StatusOK)
+		})).ServeHTTP(w, r)
+		assert.True(t, called)
+		assert.Empty(t, w.Header()["X-Header"])
 	})
 
 	t.Run("Double call write header", func(t *testing.T) {
 		r := httptest.NewRequest("GET", "/", nil)
 		w := httptest.NewRecorder()
 		called := false
-		AddResponse("X-Header", "1").ServeHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		DeleteResponse("X-Header").ServeHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			called = true
+			w.Header().Set("X-Header", "0")
 			w.WriteHeader(http.StatusOK)
 			w.WriteHeader(http.StatusNotFound)
 		})).ServeHTTP(w, r)
 		assert.True(t, called)
-		assert.Equal(t, "1", w.Header().Get("X-Header"))
+		assert.Empty(t, w.Header()["X-Header"])
 		assert.Equal(t, http.StatusOK, w.Code)
 	})
 
@@ -87,12 +95,13 @@ func TestResponseAdder(t *testing.T) {
 		r := httptest.NewRequest("GET", "/", nil)
 		w := httptest.NewRecorder()
 		called := false
-		AddResponse("X-Header", "1").ServeHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		DeleteResponse("X-Header").ServeHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			called = true
+			w.Header().Set("X-Header", "0")
 			w.Write([]byte("test"))
 		})).ServeHTTP(w, r)
 		assert.True(t, called)
-		assert.Equal(t, "1", w.Header().Get("X-Header"))
+		assert.Empty(t, w.Header()["X-Header"])
 		assert.Equal(t, http.StatusOK, w.Code)
 		assert.Equal(t, "test", w.Body.String())
 	})
