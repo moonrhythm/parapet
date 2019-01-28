f9787358f110286f2799232929229ff12f106552
Thanatat Tamtan <acoshift@gmail.com>
2019-01-28 18:06:53 +0700

    location: refactor, add general matcher (#66)

---
+++ pkg/location/matcher_test.go
@@ -0,0 +1,76 @@
+package location_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/moonrhythm/parapet"
+	. "github.com/moonrhythm/parapet/pkg/location"
+)
+
+func TestMatcher(t *testing.T) {
+	t.Parallel()
+
+	t.Run("Match", func(t *testing.T) {
+		m := New(func(r *http.Request) bool {
+			assert.NotNil(t, r)
+			return true
+		})
+
+		called := false
+		m.Use(parapet.MiddlewareFunc(func(h http.Handler) http.Handler {
+			return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				called = true
+			})
+		}))
+
+		r := httptest.NewRequest("GET", "/", nil)
+		w := httptest.NewRecorder()
+		m.ServeHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			assert.Fail(t, "must not be called")
+		})).ServeHTTP(w, r)
+		assert.True(t, called)
+	})
+
+	t.Run("Not Match", func(t *testing.T) {
+		m := New(func(r *http.Request) bool {
+			assert.NotNil(t, r)
+			return false
+		})
+
+		m.Use(parapet.MiddlewareFunc(func(h http.Handler) http.Handler {
+			return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				assert.Fail(t, "must not be called")
+			})
+		}))
+
+		r := httptest.NewRequest("GET", "/", nil)
+		w := httptest.NewRecorder()
+		called := false
+		m.ServeHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			called = true
+		})).ServeHTTP(w, r)
+		assert.True(t, called)
+	})
+
+	t.Run("Catch-all", func(t *testing.T) {
+		m := New(nil)
+
+		called := false
+		m.Use(parapet.MiddlewareFunc(func(h http.Handler) http.Handler {
+			return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				called = true
+			})
+		}))
+
+		r := httptest.NewRequest("GET", "/", nil)
+		w := httptest.NewRecorder()
+		m.ServeHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			assert.Fail(t, "must not be called")
+		})).ServeHTTP(w, r)
+		assert.True(t, called)
+	})
+}
--- pkg/location/prefix.go
+++ pkg/location/prefix.go
@@ -3,41 +3,15 @@ package location
 import (
 	"net/http"
 	"strings"
-
-	"github.com/moonrhythm/parapet"
 )
 
 // Prefix creates new prefix matcher
-func Prefix(pattern string) *PrefixMatcher {
-	return &PrefixMatcher{Pattern: pattern}
-}
-
-// PrefixMatcher matches location using prefix string
-type PrefixMatcher struct {
-	Pattern string
-	ms      parapet.Middlewares
-}
-
-// Use uses middleware
-func (l *PrefixMatcher) Use(m parapet.Middleware) {
-	l.ms.Use(m)
-}
-
-// ServeHandler implements middleware interface
-func (l PrefixMatcher) ServeHandler(h http.Handler) http.Handler {
-	next := l.ms.ServeHandler(http.NotFoundHandler())
-
-	// catch-all
-	if l.Pattern == "" {
-		return next
+func Prefix(pattern string) *Matcher {
+	if pattern == "" {
+		return New(nil)
 	}
 
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if !strings.HasPrefix(r.URL.Path, l.Pattern) {
-			h.ServeHTTP(w, r)
-			return
-		}
-
-		next.ServeHTTP(w, r)
+	return New(func(r *http.Request) bool {
+		return strings.HasPrefix(r.URL.Path, pattern)
 	})
 }
