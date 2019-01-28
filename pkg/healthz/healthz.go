45cba7a16f8304ba97db46250f3668c6dfc5caa3
acoshift <acoshift@gmail.com>
2019-01-28 19:41:35 +0700

    healthz: add shutdown logic

--- pkg/healthz/healthz.go
+++ pkg/healthz/healthz.go
@@ -1,6 +1,12 @@
 package healthz
 
-import "net/http"
+import (
+	"net/http"
+	"sync"
+	"sync/atomic"
+
+	"github.com/moonrhythm/parapet"
+)
 
 // Healthz middleware
 type Healthz struct{}
@@ -12,7 +18,27 @@ func New() *Healthz {
 
 // ServeHandler implements middleware interface
 func (m Healthz) ServeHandler(h http.Handler) http.Handler {
+	var (
+		once     sync.Once
+		shutdown int32
+	)
+
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		once.Do(func() {
+			if srv, ok := r.Context().Value(parapet.ServerContextKey).(*parapet.Server); ok {
+				srv.RegisterOnShutdown(func() {
+					atomic.StoreInt32(&shutdown, 1)
+				})
+			}
+		})
+
+		p := atomic.LoadInt32(&shutdown)
+		if p > 0 {
+			w.WriteHeader(http.StatusServiceUnavailable)
+			w.Write([]byte("Service Unavailable"))
+			return
+		}
+
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
 	})
--- pkg/healthz/healthz_test.go
+++ pkg/healthz/healthz_test.go
@@ -2,11 +2,12 @@ package healthz_test
 
 import (
 	"net/http"
-	"net/http/httptest"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 
+	"github.com/moonrhythm/parapet"
 	. "github.com/moonrhythm/parapet/pkg/healthz"
 )
 
@@ -14,10 +15,24 @@ func TestHealthz(t *testing.T) {
 	t.Parallel()
 
 	m := New()
+	s := parapet.Server{
+		Addr:               ":10100",
+		WaitBeforeShutdown: 200 * time.Millisecond,
+	}
+	defer s.Shutdown()
+	s.Use(m)
+	go s.ListenAndServe()
+	time.Sleep(50 * time.Millisecond)
 
-	r := httptest.NewRequest("GET", "/", nil)
-	w := httptest.NewRecorder()
+	resp, err := http.Get("http://127.0.0.1:10100")
+	if assert.NoError(t, err) {
+		assert.EqualValues(t, http.StatusOK, resp.StatusCode)
+	}
 
-	m.ServeHandler(http.NotFoundHandler()).ServeHTTP(w, r)
-	assert.EqualValues(t, 200, w.Code)
+	go s.Shutdown()
+	time.Sleep(20 * time.Millisecond)
+	resp, err = http.Get("http://127.0.0.1:10100")
+	if assert.NoError(t, err) {
+		assert.EqualValues(t, http.StatusServiceUnavailable, resp.StatusCode)
+	}
 }
