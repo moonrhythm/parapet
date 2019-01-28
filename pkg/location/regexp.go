f1f9137fcfa360847616eee8df32ec8d85c73e97
acoshift <acoshift@gmail.com>
2019-01-28 18:00:09 +0700

    location: refactor

--- pkg/location/regexp.go
+++ pkg/location/regexp.go
@@ -3,43 +3,17 @@ package location
 import (
 	"net/http"
 	"regexp"
-
-	"github.com/moonrhythm/parapet"
 )
 
 // RegExp creates new RegExp matcher
-func RegExp(pattern string) *RegExpMatcher {
-	return &RegExpMatcher{Pattern: pattern}
-}
-
-// RegExpMatcher matches location using regexp
-type RegExpMatcher struct {
-	Pattern string
-	ms      parapet.Middlewares
-}
-
-// Use uses middleware
-func (l *RegExpMatcher) Use(m parapet.Middleware) {
-	l.ms.Use(m)
-}
-
-// ServeHandler implements middleware interface
-func (l RegExpMatcher) ServeHandler(h http.Handler) http.Handler {
-	next := l.ms.ServeHandler(http.NotFoundHandler())
-
-	// catch-all
-	if l.Pattern == "" {
-		return next
+func RegExp(pattern string) *Matcher {
+	if pattern == "" {
+		return New(nil)
 	}
 
-	re := regexp.MustCompile(l.Pattern)
+	re := regexp.MustCompile(pattern)
 
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if !re.MatchString(r.URL.Path) {
-			h.ServeHTTP(w, r)
-			return
-		}
-
-		next.ServeHTTP(w, r)
+	return New(func(r *http.Request) bool {
+		return re.MatchString(r.URL.Path)
 	})
 }
