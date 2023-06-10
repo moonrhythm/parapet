3cfdaa92b69ade41d2187a73dc55def0f8dd8a03
Thanatat Tamtan <acoshift@gmail.com>
2023-06-10 18:01:41 +0700

    compress: allow config level

--- pkg/compress/gzip.go
+++ pkg/compress/gzip.go
@@ -7,9 +7,13 @@ import (
 
 // Gzip creates new gzip compress middleware
 func Gzip() *Compress {
+	return GzipWithLevel(gzip.DefaultCompression)
+}
+
+func GzipWithLevel(level int) *Compress {
 	return &Compress{
 		New: func() Compressor {
-			g, err := gzip.NewWriterLevel(io.Discard, gzip.DefaultCompression)
+			g, err := gzip.NewWriterLevel(io.Discard, level)
 			if err != nil {
 				panic(err)
 			}
