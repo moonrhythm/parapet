c944907cd5c5e9dea75dfd15b76fa6452f3dfffe
Thanatat Tamtan <acoshift@gmail.com>
2023-07-20 20:38:01 +0700

    trace: move trace out from stackdriver (#149)

---
+++ pkg/trace/trace.go
@@ -0,0 +1,47 @@
+package trace
+
+import (
+	"net/http"
+
+	"go.opencensus.io/plugin/ochttp"
+	"go.opencensus.io/trace"
+	"go.opencensus.io/trace/propagation"
+
+	"github.com/moonrhythm/parapet/pkg/header"
+)
+
+// New creates new trace middleware
+func New() *Trace {
+	return new(Trace)
+}
+
+// Trace middleware
+//
+//nolint:govet
+type Trace struct {
+	Propagation      propagation.HTTPFormat
+	IsPublicEndpoint bool
+	FormatSpanName   func(r *http.Request) string
+	StartOptions     trace.StartOptions
+}
+
+// ServeHandler implements middleware interface
+func (m Trace) ServeHandler(h http.Handler) http.Handler {
+	if m.FormatSpanName == nil {
+		m.FormatSpanName = func(r *http.Request) string {
+			proto := header.Get(r.Header, header.XForwardedProto)
+			return proto + "://" + r.Host + r.RequestURI
+		}
+	}
+	if m.StartOptions.SpanKind == trace.SpanKindUnspecified {
+		m.StartOptions.SpanKind = trace.SpanKindServer
+	}
+
+	return &ochttp.Handler{
+		Handler:          h,
+		Propagation:      m.Propagation,
+		FormatSpanName:   m.FormatSpanName,
+		StartOptions:     m.StartOptions,
+		IsPublicEndpoint: m.IsPublicEndpoint,
+	}
+}
